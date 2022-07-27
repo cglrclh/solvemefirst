8b58c930628f74c2dcc6c04173e107ffc0bbea40
cglrclh <caglarculha35@gmail.com>
2022-07-27 15:53:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+        int toplam = sayi1 + sayi2;
+        Console.WriteLine(toplam);
+    }
+}
